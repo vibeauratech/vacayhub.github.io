77dc7ba6b8c671e1f512c4d8ccaa58d7003c91ef
vibeauratech <emmanuelakharia@gmail.com>
2022-12-22 14:25:38 +0100

    yes

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const linkImages = document.querySelectorAll('.hidden-img');
 for (let i = 0; i < link.length; i++) {
     link[i].addEventListener('mousemove', (e) =>{
       hoverReveal[i].style.opacity = 1;
-      hoverReveal[i].style.transform = `translate(-100%,-50%) rotate(5deg)`;
+      hoverReveal[i].style.transform = `translate(-100%,-50%) `;
       linkImages[i].style.transform = 'scale(1,1)';
       hoverReveal[i].style.left = e.clientX + "px";
     })
 
     link[i].addEventListener('mouseleave', (e) =>{
         hoverReveal[i].style.opacity = 0;
-        hoverReveal[i].style.transform = `translate(-50%,-50%) rotate(-5deg)`;
+        hoverReveal[i].style.transform = `translate(-50%,-50%) `;
         linkImages[i].style.transform = 'scale(0.8,0.8)';
     })
 }
