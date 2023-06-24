3bee82cecb544ea9080097cd3053347549fe6576
emmanuelakharia <107748497+vibeauratech@users.noreply.github.com>
2023-06-24 23:21:32 +0100

    yes

--- preload.js
+++ preload.js
@@ -83,18 +83,26 @@ gsap.to(".box", {
     delay: 0.3
 })
 
-gsap.from("#domino", {
-    scrollTrigger :{
-      trigger:"#domino",
-      toggleActions: "restart none none none " 
-    }, 
+var textWrapper = document.querySelector('.ml13');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-    y: 50,
-    opacity : 0,
-    ease :" power4.inOut",
-    delay: 1
-})
-
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml13 .letter',
+    translateY: [100,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 300 + 30 * i
+  }).add({
+    targets: '.ml13 .letter',
+    translateY: [0,-100],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1200,
+    delay: (el, i) => 100 + 30 * i
+  });
 
 
 
